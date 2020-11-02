0fb4c9b7f8e3578c66bfd0053910039b8d532701
williamwanjia <williamwanjia@gmail.com>
2020-11-02 17:46:58 +0100

    Init

---
+++ install_ros.sh
@@ -0,0 +1,14 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-get update
+sudo apt-get install -y ros-kinetic-desktop-full
+
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install -y python-rosdep
+sudo rosdep init
+rosdep update
+
+
